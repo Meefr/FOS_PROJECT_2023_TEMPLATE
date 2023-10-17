b6952702a27c5bde61ba19c88dc59e9bdb48da13
Meefr <mohamedelramah737@gmail.com>
2023-10-17 21:43:54 +0300

    add str2lower first time meefr edit

--- kern/cmd/command_prompt.c
+++ kern/cmd/command_prompt.c
@@ -379,6 +379,7 @@ int process_command(int number_of_arguments, char** arguments)
 {
 	//TODO: [PROJECT'23.MS1 - #2] [1] PLAY WITH CODE! - process_command
 	//Comment the following line before start coding...
-	panic("process_command is not implemented yet");
+//	panic("process_command is not implemented yet");
+
 	return 0;
 }
