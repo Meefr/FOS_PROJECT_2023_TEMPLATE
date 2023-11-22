d178a264232dbe5c99e5ec80e5c59c55cc63ff80
Amrkhaled5 <amrk4415@gmail.com>
2023-11-22 19:09:06 +0200

    ready for user

---
+++ kern/mem/kheap.c
@@ -0,0 +1,373 @@
+#include "kheap.h"
+
+#include <inc/memlayout.h>
+#include <inc/dynamic_allocator.h>
+#include "memory_manager.h"
+
+#define num_of_all_pages 32722
+int initialize_kheap_dynamic_allocator(uint32 daStart,
+		uint32 initSizeToAllocate, uint32 daLimit) {
+
+	start = daStart;
+	hLimit = daLimit;
+	segmentbrk = start + initSizeToAllocate;
+	segmentbrk = ROUNDUP(segmentbrk, PAGE_SIZE);
+	if ((initSizeToAllocate > daLimit)
+			|| ((daStart + initSizeToAllocate) > daLimit))
+		return E_NO_MEM;
+	// handle if size = limit > sbrk
+	// 0-4 // 4-8 // 8-12
+	for (uint32 i = daStart; i < daStart + initSizeToAllocate; i += PAGE_SIZE) {
+//		uint32 *pageTable;
+//		struct Frame_Info *ptr=get_frame_info(ptr_page_directory,(void*)i,&pageTable);
+//		if(ptr!=NULL){
+//			continue;
+//		}
+		struct FrameInfo* ptrr;
+		int ret = allocate_frame(&ptrr);
+		ptrr->va = i;
+		if (ret == E_NO_MEM) {
+			return E_NO_MEM;
+		}
+		map_frame(ptr_page_directory, ptrr, i, PERM_WRITEABLE);
+
+	}
+	// access null in this fun call
+	initialize_dynamic_allocator(daStart, initSizeToAllocate);
+	return 0;
+}
+
+void* sbrk(int increment) {
+	//TODO: [PROJECT'23.MS2 - #02] [1] KERNEL HEAP - sbrk()
+	/* increment > 0: move the segment break of the kernel to increase the size of its heap,
+	 * 				you should allocate pages and map them into the kernel virtual address space as necessary,
+	 * 				and returns the address of the previous break (i.e. the beginning of newly mapped memory).
+	 * increment = 0: just return the current position of the segment break
+	 * increment < 0: move the segment break of the kernel to decrease the size of its heap,
+	 * 				you should deallocate pages that no longer contain part of the heap as necessary.
+	 * 				and returns the address of the new break (i.e. the end of the current heap space).
+	 *
+	 * NOTES:
+	 * 	1) You should only have to allocate or deallocate pages if the segment break crosses a page boundary
+	 * 	2) New segment break should be aligned on page-boundary to avoid "No Man's Land" problem
+	 * 	3) Allocating additional pages for a kernel dynamic allocator will fail if the free frames are exhausted
+	 * 		or the break exceed the limit of the dynamic allocator. If sbrk fails, kernel should panic(...)
+	 */
+
+	//MS2: COMMENT THIS LINE BEFORE START CODING====
+	// edit in ms1 not finished yet
+	if (increment > 0) {
+		// roundup then check if in the hard boundries
+		// should i pass page_size or just 4
+		increment = ROUNDUP(increment, PAGE_SIZE);
+		uint32 prevSbrk = segmentbrk;
+		if (segmentbrk + increment <= hLimit) {
+			segmentbrk += increment;
+
+			for (uint32 i = prevSbrk; i < segmentbrk; i += (PAGE_SIZE)) {
+				struct FrameInfo* ptrr;
+				int ret = allocate_frame(&ptrr);
+				ptrr->va = i;
+				if (ret == E_NO_MEM) {
+					return (void*) E_NO_MEM;
+				}
+				map_frame(ptr_page_directory, ptrr, i, PERM_WRITEABLE);
+
+			}
+		} else {
+			panic("in sbrk func increment>0");
+		}
+		return (void*) prevSbrk;
+	} else if (increment == 0) {
+		return (void*) segmentbrk;
+	} else if (increment < 0) {
+		// dec sbrk to increment
+		// if inc=10 , should i free 3 pages (12) or only 2 pages(8)
+		//increment = ROUNDUP(-increment, PAGE_SIZE);
+		uint32 newSbrk = segmentbrk - ((increment / PAGE_SIZE) * PAGE_SIZE);
+		if (newSbrk < start) {
+			panic("in sbrk func increment<0 and newSbrk<start");
+		}
+		for (uint32 i = segmentbrk; i > newSbrk; i -= (PAGE_SIZE)) {
+			unmap_frame(ptr_page_directory, i);
+//			free_frame((struct FrameInfo*) i);
+		}
+		segmentbrk -= ((increment / PAGE_SIZE) * PAGE_SIZE);
+		return (void*) newSbrk;
+	}
+	return (void*) -1;
+
+}
+
+void* kmalloc(unsigned int size) {
+	//TODO: [PROJECT'23.MS2 - #03] [1] KERNEL HEAP - kmalloc()
+	//refer to the project presentation and documentation for details
+	// use "isKHeapPlacementStrategyFIRSTFIT() ..." functions to check the current strategy
+
+	//change this "return" according to your answer
+//	kpanic_into_prompt("kmalloc() is not implemented yet...!!");
+	// 16
+//	cprintf("free pages number: %d, non free pages %d\n",tmp, ((KERNEL_HEAP_MAX - (hLimit + PAGE_SIZE))/PAGE_SIZE)-tmp);
+	//here we need to know roundUp where ?
+	//cprintf("%d \n",(hLimit + PAGE_SIZE));
+	if (size <= DYN_ALLOC_MAX_BLOCK_SIZE) {
+		if (isKHeapPlacementStrategyFIRSTFIT()) {
+			// here we need type cast;
+			return alloc_block_FF(size);
+		}
+		if (isKHeapPlacementStrategyBESTFIT()) {
+			return alloc_block_BF(size);
+		}
+	} else {
+		//need cast ?
+		uint32 new_size = ROUNDUP(size, PAGE_SIZE);
+		int number_of_pages = new_size / PAGE_SIZE;
+		int count = 0;
+		struct FrameInfo * ptr_fram_Info;
+		uint32 *pageTable;
+		uint32 address;
+		// need to rev?
+		for (uint32 i = (hLimit + PAGE_SIZE); i < KERNEL_HEAP_MAX; i +=
+				(PAGE_SIZE)) {
+
+//			ptr_fram_Info = get_frame_info(&ptr_page_directory, &i,
+//					&pageTable);
+			ptr_fram_Info = get_frame_info(ptr_page_directory, i, &pageTable);
+			uint32 page_table_entry = pageTable[PTX(i)];
+//			cprintf("entry: %x\npresent: %x\n",page_table_entry,presentBit);
+			if (!(page_table_entry & PERM_PRESENT))
+				count++;
+			else {
+				count = 0;
+			}
+
+			if (count == number_of_pages) {
+				i = i - ((number_of_pages - 1) * PAGE_SIZE);
+				address = i;
+				for (uint32 j = 0; j < number_of_pages; j++) {
+					struct FrameInfo* ptrr;
+					int ret = allocate_frame(&ptrr);
+					ptrr->va = i;
+					if (ret == E_NO_MEM) {
+						return (void*) E_NO_MEM;
+					}
+					ret = map_frame(ptr_page_directory, ptrr, i,PERM_WRITEABLE);
+					if (ret == E_NO_MEM) {
+						unmap_frame(ptr_page_directory, i);
+						return (void*) E_NO_MEM;
+					}
+					i += PAGE_SIZE;
+				}
+//				struct vmBlock *ptr;
+//				LIST_INSERT_HEAD(&vmBlocks, ptr);
+				for (int j = 0; j < num_of_all_pages; j++) {
+					if (vmS[j] == 0) {
+						vmS[j] = address;
+						numOfPages[j] = number_of_pages;
+//						cprintf("number of all pages %d \n", (KERNEL_HEAP_MAX - (hLimit+ PAGE_SIZE))/ PAGE_SIZE);
+//						cprintf("%d ,%d, %d\n", vmS[j], numOfPages[j],
+//								number_of_pages);
+						break;
+					}
+				}
+//				if(address == -134197248 || address == -131051520 || address == -130002944)
+//					cprintf("%d ,%d, %d\n",address,size,number_of_pages);
+				return (void *) address;
+			}
+		}
+	}
+
+	return NULL;
+}
+
+void kfree(void* virtual_address) {
+	//TODO: [PROJECT'23.MS2 - #04] [1] KERNEL HEAP - kfree()
+	//refer to the project presentation and documentation for details
+	// Write your code here, remove the panic and write your code
+	//panic("kfree() is not implemented yet...!!");
+	if ((uint32) virtual_address >= start
+			&& (uint32) virtual_address < hLimit) {
+		free_block(virtual_address);
+		return;
+	} else if ((uint32) virtual_address >= hLimit + PAGE_SIZE
+			&& (uint32) virtual_address < KERNEL_HEAP_MAX) {
+//		virtual_address = (void *)ROUNDDOWN((uint32)virtual_address,PAGE_SIZE);
+		for (int i = 0; i < num_of_all_pages; i++) {
+			if (vmS[i] == (uint32) virtual_address) {
+				for (int j = 0; j < numOfPages[i]; j++) {
+//					cprintf("%d \n",(vmS[i] + (PAGE_SIZE * j)));
+					unmap_frame(ptr_page_directory, (vmS[i] + (PAGE_SIZE * j)));
+				}
+				vmS[i] = 0;
+				numOfPages[i] = 0;
+				return;
+			}
+		}
+	}
+//	panic("invalid virtual address");
+}
+
+unsigned int kheap_virtual_address(unsigned int physical_address) {
+	//TODO: [PROJECT'23.MS2 - #05] [1] KERNEL HEAP - kheap_virtual_address()
+	//refer to the project presentation and documentation for details
+	// Write your code here, remove the panic and write your code
+	//panic("kheap_virtual_address() is not implemented yet...!!");
+	//EFFICIENT IMPLEMENTATION ~O(1) IS REQUIRED ==================
+
+	struct FrameInfo *ptr_frame_info;
+	ptr_frame_info = to_frame_info((uint32)physical_address);
+	if (ptr_frame_info->references > 0)
+		{
+		uint32 offset = physical_address;
+		offset = offset << 20;
+		offset = offset >> 20;
+		return (unsigned int)ptr_frame_info->va + offset;
+		}
+	//change this "return" according to your answer
+	return 0;
+}
+
+unsigned int kheap_physical_address(unsigned int virtual_address) {
+	//TODO: [PROJECT'23.MS2 - #06] [1] KERNEL HEAP - kheap_physical_address()
+	//refer to the project presentation and documentation for details
+	// Write your code here, remove the panic and write your code
+	//panic("kheap_physical_address() is not implemented yet...!!");
+
+	//change this "return" according to your answer
+//	ptr_fram_Info = get_frame_info(ptr_page_directory,i,&pageTable);
+//	uint32 page_table_entry = pageTable[PTX(i)];
+//	uint32 presentBit = page_table_entry;
+//	presentBit << 31;
+//	presentBit >> 31;
+
+	uint32* ptr;
+	int check = get_page_table(ptr_page_directory, virtual_address, &ptr);
+
+	if (check == TABLE_IN_MEMORY) {
+		uint32 entry = ptr[PTX(virtual_address)];
+		//uint32 offset=entry%PAGE_SIZE;
+		uint32 ans = ((entry >> 12) * PAGE_SIZE)
+				+ (virtual_address & 0x00000FFF);
+		//cprintf("the pa is %x",ans);
+		return (int) (ans);
+	}
+	return 0;
+}
+
+void kfreeall() {
+	panic("Not implemented!");
+
+}
+
+void kshrink(uint32 newSize) {
+	panic("Not implemented!");
+}
+
+void kexpand(uint32 newSize) {
+	panic("Not implemented!");
+}
+
+//=================================================================================//
+//============================== BONUS FUNCTION ===================================//
+//=================================================================================//
+// krealloc():
+
+//	Attempts to resize the allocated space at "virtual_address" to "new_size" bytes,
+//	possibly moving it in the heap.
+//	If successful, returns the new virtual_address, in which case the old virtual_address must no longer be accessed.
+//	On failure, returns a null pointer, and the old virtual_address remains valid.
+
+//	A call with virtual_address = null is equivalent to kmalloc().
+//	A call with new_size = zero is equivalent to kfree().
+
+void *krealloc(void *virtual_address, uint32 new_size) {
+	//TODO: [PROJECT'23.MS2 - BONUS#1] [1] KERNEL HEAP - krealloc()
+	// Write your code here, remove the panic and write your code
+	if(virtual_address==NULL){
+		return kmalloc(new_size);
+	}
+	else if(new_size==0){
+		kfree(virtual_address);
+	}
+	else{
+		if(new_size<= DYN_ALLOC_MAX_BLOCK_SIZE){
+			return realloc_block_FF(virtual_address,new_size);
+		}
+		else{
+			uint32 NewNumberOfPages=ROUNDUP(new_size,PAGE_SIZE)/PAGE_SIZE;
+			uint32 NumberOfCurrentPages=0;
+			int indexOfva;
+			for (int i = 0; i < num_of_all_pages; i++) {
+				if (vmS[i] == (uint32) virtual_address) {
+					NumberOfCurrentPages=numOfPages[i];
+					indexOfva=i;
+					break;
+				}
+			}
+			// the va may not found in the array ?
+			if(NumberOfCurrentPages==0){
+				cprintf("aaaa0\n");
+				return kmalloc(NewNumberOfPages);
+			}
+			else{
+				cprintf("va %d , CurrentPages %d , NewNumberOfPages %d \n",virtual_address,NumberOfCurrentPages,NewNumberOfPages);
+				if(NumberOfCurrentPages==NewNumberOfPages){
+					//numOfPages[indexOfva]=NewNumberOfPages;
+					cprintf("aaaa1\n");
+					return virtual_address;
+				}
+				else if(NumberOfCurrentPages>NewNumberOfPages){
+					cprintf("aaaa2\n");
+//					uint32 NumberOfPagesToDelet=(NumberOfCurrentPages-NewNumberOfPages);
+//					uint32 StartPtr=(uint32)virtual_address+(NewNumberOfPages*PAGE_SIZE);
+//					numOfPages[indexOfva]=NewNumberOfPages;
+//					for(uint32 i=0;i<NumberOfPagesToDelet;i++){
+//						unmap_frame(ptr_page_directory, (StartPtr + (PAGE_SIZE * i)));
+//					}
+					return virtual_address;
+				}
+				else{
+					cprintf("aaaa3\n");
+					struct FrameInfo * ptr_fram_Info;
+					uint32 *pageTable;
+					uint32 NumberOfPagesNeedToalloc=NewNumberOfPages-NumberOfCurrentPages;
+					int count=0;
+					uint32 address;
+					for(uint32 i=(uint32)virtual_address+(PAGE_SIZE*NumberOfCurrentPages);
+							i<(uint32)virtual_address+(PAGE_SIZE*NewNumberOfPages);i+=PAGE_SIZE){
+						ptr_fram_Info = get_frame_info(ptr_page_directory, i, &pageTable);
+						uint32 page_table_entry = pageTable[PTX(i)];
+						if (!(page_table_entry & PERM_PRESENT))
+							count++;
+						else {
+							kfree(virtual_address);
+
+
+							return kmalloc(new_size);
+						}
+					}
+					if(count>0){
+						numOfPages[indexOfva]=NewNumberOfPages;
+						for(uint32 i=(uint32)virtual_address+(PAGE_SIZE*NumberOfCurrentPages);
+						i<(uint32)virtual_address+(PAGE_SIZE*NewNumberOfPages);i+=PAGE_SIZE){
+							struct FrameInfo* ptrr;
+							int ret = allocate_frame(&ptrr);
+							ptrr->va=i;
+							if (ret == E_NO_MEM) {
+								return (void*) E_NO_MEM;
+							}
+							ret = map_frame(ptr_page_directory, ptrr, i,PERM_WRITEABLE);
+							if (ret == E_NO_MEM) {
+								unmap_frame(ptr_page_directory, i);
+								return (void*) E_NO_MEM;
+							}
+						}
+						return virtual_address;
+					}
+				}
+			}
+		}
+	}
+	return NULL;
+	//panic("krealloc() is not implemented yet...!!");
+}
---
+++ lib/dynamic_allocator.c
@@ -0,0 +1,429 @@
+/*
+ * dynamic_allocator.c
+ *
+ *  Created on: Sep 21, 2023
+ *      Author: HP
+ */
+#include <inc/assert.h>
+#include <inc/string.h>
+#include "../inc/dynamic_allocator.h"
+
+//==================================================================================//
+//============================== GIVEN FUNCTIONS ===================================//
+//==================================================================================//
+
+//=====================================================
+// 1) GET BLOCK SIZE (including size of its meta data):
+//=====================================================
+
+uint32 get_block_size(void* va) {
+	struct BlockMetaData *curBlkMetaData = ((struct BlockMetaData *) va - 1);
+	return curBlkMetaData->size;
+}
+
+//===========================
+// 2) GET BLOCK STATUS:
+//===========================
+
+int8 is_free_block(void* va) {
+	struct BlockMetaData *curBlkMetaData = ((struct BlockMetaData *) va - 1);
+	return curBlkMetaData->is_free;
+}
+
+//===========================================
+// 3) ALLOCATE BLOCK BASED ON GIVEN STRATEGY:
+//===========================================
+void *alloc_block(uint32 size, int ALLOC_STRATEGY) {
+	void *va = NULL;
+	switch (ALLOC_STRATEGY) {
+	case DA_FF:
+		va = alloc_block_FF(size);
+		break;
+	case DA_NF:
+		va = alloc_block_NF(size);
+		break;
+	case DA_BF:
+		va = alloc_block_BF(size);
+		break;
+	case DA_WF:
+		va = alloc_block_WF(size);
+		break;
+	default:
+		cprintf("Invalid allocation strategy\n");
+		break;
+	}
+	return va;
+}
+
+//===========================
+// 4) PRINT BLOCKS LIST:
+//===========================
+
+void print_blocks_list(struct MemBlock_LIST list) {
+	cprintf("=========================================\n");
+	struct BlockMetaData* blk;
+	cprintf("\nDynAlloc Blocks List:\n");
+	LIST_FOREACH(blk, &list)
+	{
+		cprintf("(size: %d, isFree: %d)\n", blk->size, blk->is_free);
+	}
+	cprintf("=========================================\n");
+
+}
+//
+////********************************************************************************//
+////********************************************************************************//
+
+//==================================================================================//
+//============================ REQUIRED FUNCTIONS ==================================//
+//==================================================================================//
+bool is_initialized = 0;
+//==================================
+// [1] INITIALIZE DYNAMIC ALLOCATOR:
+//==================================
+void initialize_dynamic_allocator(uint32 daStart,
+		uint32 initSizeOfAllocatedSpace) {
+	//=========================================
+	//DON'T CHANGE THESE LINES=================
+	if (initSizeOfAllocatedSpace == 0)
+		return;
+
+
+	is_initialized = 1;
+	//=========================================
+	//=========================================
+	//TODO: [PROJECT'23.MS1 - #5] [3] DYNAMIC ALLOCATOR - initialize_dynamic_allocator()
+	//	panic("initialize_dynamic_allocator is not implemented yet");
+	struct BlockMetaData *head = (struct BlockMetaData *) daStart;
+	head->size = initSizeOfAllocatedSpace;
+	head->is_free = 1;
+	LIST_INSERT_HEAD(&memBlocks, head);
+}
+
+//=========================================
+// [4] ALLOCATE BLOCK BY FIRST FIT:
+//=========================================
+void *alloc_block_FF(uint32 size) {
+	//
+	//TODO: [PROJECT'23.MS1 - #6] [3] DYNAMIC ALLOCATOR - alloc_block_FF()
+	//	panic("alloc_block_FF is not implemented yet");
+	if (size == 0)
+		return NULL;
+
+	// code update for MS2
+	if (!is_initialized) {
+		uint32 required_size = size + sizeOfMetaData();
+		uint32 da_start = (uint32) sbrk(required_size);
+		//get new break since it's page aligned! thus, the size can be more than the required one
+		uint32 da_break = (uint32) sbrk(0);
+		initialize_dynamic_allocator(da_start, da_break - da_start);
+	}
+
+	struct BlockMetaData *blk, *tmpBlk;
+	//	tmpBlk->size = 0;
+	LIST_FOREACH(blk, &memBlocks)
+	{
+		//blk size is found -> allocate
+		if ((blk->size - sizeOfMetaData()) >= size && blk->is_free == 1) {
+//		cprintf("sn: %d , sb: %d \n",(size +sizeOfMetaData()) , blk->size);
+
+			//blk size is not enough to hold data -> no split
+			if ((blk->size - (sizeOfMetaData() + size)) <= sizeOfMetaData()) {
+//				cprintf("---------second if---------");
+				blk->is_free = 0;
+				return (struct BlockMetaData *) ((uint32) blk + sizeOfMetaData());
+			}
+			//blk size is big enough to hold data -> split
+			else {
+//				cprintf("---------else---------");
+				tmpBlk = blk;
+				blk = (struct BlockMetaData *) ((uint32) blk
+						+ (size + sizeOfMetaData()));
+				blk->size = tmpBlk->size - (size + sizeOfMetaData());
+				blk->is_free = 1;
+
+//				cprintf("blk: %x\ntmp: %x\n", blk, tmpBlk);
+				LIST_INSERT_AFTER(&memBlocks, tmpBlk, blk);
+				tmpBlk->size = size + sizeOfMetaData();
+				tmpBlk->is_free = 0;
+				return (struct BlockMetaData *) ((uint32) tmpBlk
+						+ sizeOfMetaData());
+//				 tmpBlk = blk;
+//				                struct BlockMetaData *newBlk = (struct BlockMetaData *)((uint32)blk + (size + sizeOfMetaData()));
+//				                newBlk->size = tmpBlk->size - (size + sizeOfMetaData());
+//				                newBlk->is_free = 1;
+//				                LIST_INSERT_AFTER(&memBlocks, tmpBlk, newBlk);
+//				                tmpBlk->size = size + sizeOfMetaData();
+//				                tmpBlk->is_free = 0;
+//				                return (struct BlockMetaData *)((uint32)tmpBlk + sizeOfMetaData());
+			}
+		}
+	}
+	//no free space for required size -> no allocate + no space
+	uint32* ptr = (uint32 *) sbrk((size + sizeOfMetaData()));
+	if (ptr != (uint32 *) -1) {
+		blk = (struct BlockMetaData *) memBlocks.lh_last;
+		if (blk->is_free == 1) {
+			blk->size = size + blk->size + sizeOfMetaData();
+		 	tmpBlk = blk;
+			blk = (struct BlockMetaData *) ((uint32) blk
+					+ (size + sizeOfMetaData()));
+			blk->size = tmpBlk->size - (size + sizeOfMetaData());
+			blk->is_free = 1;
+			//				cprintf("blk: %x\ntmp: %x\n", blk, tmpBlk);
+			LIST_INSERT_AFTER(&memBlocks, tmpBlk, blk);
+			tmpBlk->size = size + sizeOfMetaData();
+			tmpBlk->is_free = 0;
+			return (struct BlockMetaData *) ((uint32) tmpBlk + sizeOfMetaData());
+		} else {
+			tmpBlk = (struct BlockMetaData *) ptr;
+			tmpBlk->size = size + sizeOfMetaData();
+			tmpBlk->is_free = 0;
+			LIST_INSERT_TAIL(&memBlocks, tmpBlk);
+		}
+		return (struct BlockMetaData *) ((uint32) tmpBlk + sizeOfMetaData());
+	}
+	return NULL;
+
+}
+//=========================================
+// [5] ALLOCATE BLOCK BY BEST FIT:
+//=========================================
+void *alloc_block_BF(uint32 size) {
+	//TODO: [PROJECT'23.MS1 - BONUS] [3] DYNAMIC ALLOCATOR - alloc_block_BF()
+	//	panic("alloc_block_BF is not implemented yet");
+	if (size == 0)
+		return NULL;
+	struct BlockMetaData *blk, *tmpBlk, *blkAt;
+	uint32 minSize = -1;
+	//	tmpBlk->size = 0;
+	LIST_FOREACH(blk, &memBlocks)
+	{
+		//need to compaction ?
+		//blk size is found -> allocate
+		if ((blk->size - sizeOfMetaData()) >= size && blk->is_free == 1) {
+			if (minSize == -1) {
+				minSize = blk->size;
+				blkAt = blk;
+			} else if (minSize > blk->size) {
+				minSize = blk->size;
+				blkAt = blk;
+			}
+		}
+	}
+	//no free space for required size -> no allocate + no space
+	if (minSize == -1) {
+		uint32* ptr = (uint32 *) sbrk((size + sizeOfMetaData()));
+		if (ptr != (uint32 *) -1) {
+			tmpBlk = (struct BlockMetaData *) memBlocks.lh_last;
+			if (tmpBlk->is_free == 1) {
+				tmpBlk->size = size;
+				tmpBlk->is_free = 0;
+			} else {
+				tmpBlk = (struct BlockMetaData *) ptr;
+				tmpBlk->size = size + sizeOfMetaData();
+				tmpBlk->is_free = 0;
+			}
+			return (struct BlockMetaData *) ((uint32) tmpBlk + sizeOfMetaData());
+		}
+		return NULL;
+	} else {
+		//blk size is not enough to hold data -> no split
+		if ((blkAt->size - (sizeOfMetaData() + size)) < sizeOfMetaData()) {
+			blkAt->is_free = 0;
+			return (void *) ((uint32) blkAt + sizeOfMetaData());
+		}
+		//blk size is big enough to hold data -> split
+		else {
+			tmpBlk = blkAt;
+			blkAt = (struct BlockMetaData *) ((uint32) blkAt
+					+ (size + sizeOfMetaData()));
+			blkAt->size = tmpBlk->size - (size + sizeOfMetaData());
+			blkAt->is_free = 1;
+
+			//				cprintf("blk: %x\ntmp: %x\n", blk, tmpBlk);
+			LIST_INSERT_AFTER(&memBlocks, tmpBlk, blkAt);
+			tmpBlk->size = size + sizeOfMetaData();
+			tmpBlk->is_free = 0;
+			return (void *) ((uint32) tmpBlk + sizeOfMetaData());
+		}
+	}
+}
+
+//=========================================
+// [6] ALLOCATE BLOCK BY WORST FIT:
+//=========================================
+void *alloc_block_WF(uint32 size) {
+	panic("alloc_block_WF is not implemented yet");
+	return NULL;
+}
+
+//=========================================
+// [7] ALLOCATE BLOCK BY NEXT FIT:
+//=========================================
+void *alloc_block_NF(uint32 size) {
+	panic("alloc_block_NF is not implemented yet");
+	return NULL;
+}
+
+//===================================================
+// [8] FREE BLOCK WITH COALESCING:
+//===================================================
+void free_block(void *va) {
+	//TODO: [PROJECT'23.MS1 - #7] [3] DYNAMIC ALLOCATOR - free_block()
+	//	panic("free_block is not implemented yet");
+	struct BlockMetaData *ptr = ((struct BlockMetaData *) va - 1);
+	if (ptr == NULL)
+		return;
+	// ptr need to free is free -> no need to do anything
+	// invalid address -> no need to do anything
+	// check corners
+	ptr->is_free = 1;
+	// next and prev meta data is free
+	if (ptr->prev_next_info.le_prev != NULL
+			&& ptr->prev_next_info.le_next != NULL
+			&& ptr->prev_next_info.le_next->is_free == 1
+			&& ptr->prev_next_info.le_prev->is_free == 1) {
+		ptr->prev_next_info.le_prev->size = (ptr->size
+				+ ptr->prev_next_info.le_next->size
+				+ ptr->prev_next_info.le_prev->size);
+		ptr->prev_next_info.le_next->size = 0;
+		ptr->prev_next_info.le_next->is_free = 0;
+		ptr->size = 0;
+		ptr->is_free = 0;
+		struct BlockMetaData *tmp = ptr->prev_next_info.le_next;
+		LIST_REMOVE(&memBlocks, tmp);
+		LIST_REMOVE(&memBlocks, ptr);
+	}
+	// neither next or prev meta data is free
+	else if (ptr->prev_next_info.le_prev != NULL
+			&& ptr->prev_next_info.le_next != NULL
+			&& ptr->prev_next_info.le_next->is_free == 0
+			&& ptr->prev_next_info.le_prev->is_free == 0) {
+		ptr->is_free = 1;
+	}
+//			// prev meta data is free only
+	else if (ptr->prev_next_info.le_prev != NULL
+			&& ptr->prev_next_info.le_prev->is_free == 1) {
+		ptr->prev_next_info.le_prev->size = (ptr->size
+				+ ptr->prev_next_info.le_prev->size);
+		ptr->size = 0;
+		ptr->is_free = 0;
+		LIST_REMOVE(&memBlocks, ptr);
+	}
+	// next meta data is free only
+	else if (ptr->prev_next_info.le_next != NULL
+			&& ptr->prev_next_info.le_next->is_free == 1) {
+		ptr->size = (ptr->prev_next_info.le_next->size + ptr->size);
+		ptr->prev_next_info.le_next->size = 0;
+		ptr->prev_next_info.le_next->is_free = 0;
+		ptr->is_free = 1;
+		struct BlockMetaData *tmp = ptr->prev_next_info.le_next;
+		LIST_REMOVE(&memBlocks, tmp);
+	}
+}
+
+//=========================================
+// [4] REALLOCATE BLOCK BY FIRST FIT:
+//=========================================
+void *realloc_block_FF(void* va, uint32 new_size) {
+	//TODO: [PROJECT'23.MS1 - #8] [3] DYNAMIC ALLOCATOR - realloc_block_FF()
+
+	//know we have to test if i want to increase my size
+	//he test if small or equal and 3 if condetion he are tested
+
+	// new_size == 0 -> free
+	if (new_size == 0 && va != NULL) {
+		free_block(va);
+		return NULL;
+	}
+
+	// size != zero and va == NULL -> allocate the new size
+	else if (new_size != 0 && va == NULL) {
+		return alloc_block_FF(new_size);
+	}
+
+	// size == 0 and va == NULL
+	else if (new_size == 0 && va == NULL) {
+		return NULL;
+	}
+
+	// size != 0 and va != NULL
+	new_size += sizeOfMetaData();
+	struct BlockMetaData *ptr = ((struct BlockMetaData *) va - 1), *tmpBlk;
+	if (new_size < ptr->size) {
+		tmpBlk = ptr;
+		if (ptr->size - new_size >= sizeOfMetaData()) {
+			ptr = (struct BlockMetaData *) ((uint32) ptr + (new_size));
+			ptr->size = tmpBlk->size - (new_size);
+			ptr->is_free = 1;
+			tmpBlk->size = new_size;
+			/*
+			 * take care if he can reallocate a free block of data so
+			 * free function will change the needed size
+			 */
+			LIST_INSERT_AFTER(&memBlocks, tmpBlk, ptr);
+			if (tmpBlk->is_free == 1) {
+				tmpBlk->is_free = 0;
+				free_block(
+						(struct BlockMetaData *) ((uint32) ptr
+								+ sizeOfMetaData()));
+				tmpBlk->is_free = 1;
+			} else {
+				free_block(
+						(struct BlockMetaData *) ((uint32) ptr
+								+ sizeOfMetaData()));
+			}
+		}
+		return (void *) ((uint32) tmpBlk + sizeOfMetaData());
+	} else if (new_size > ptr->size) {
+		//This if not handeled in test
+		// no check if the next is free or not
+		// no test if th next is null
+		//no test if the next size < size i need (we compare next with its meta)
+		if ((ptr->prev_next_info.le_next != NULL
+				&& ptr->prev_next_info.le_next->is_free == 0)
+				|| ptr->prev_next_info.le_next == NULL
+				|| (ptr->prev_next_info.le_next != NULL
+						&& (ptr->prev_next_info.le_next->size - sizeOfMetaData()
+								< (new_size - ptr->size)))) {
+
+			free_block(((struct BlockMetaData *) ptr + 1));
+			return alloc_block_FF(new_size - sizeOfMetaData());
+
+		} else if (ptr->prev_next_info.le_next != NULL
+				&& ptr->prev_next_info.le_next->is_free == 1) {
+			if ((ptr->prev_next_info.le_next->size - sizeOfMetaData()
+					> (new_size - ptr->size))) {
+				uint32 tmpsize = ptr->prev_next_info.le_next->size;
+				struct BlockMetaData * tmpPtr = ptr->prev_next_info.le_next;
+				tmpPtr->is_free = 0;
+				tmpPtr->size = 0;
+				LIST_REMOVE(&memBlocks, tmpPtr);
+				tmpPtr = (struct BlockMetaData *) ((uint32) tmpPtr
+						+ (new_size - ptr->size));
+				tmpPtr->size = tmpsize - (new_size - ptr->size);
+				tmpPtr->is_free = 1;
+				ptr->size = new_size;
+			}
+//			LIST_INSERT_AFTER(&memBlocks,ptr->prev_next_info.le_next,tmpPtr);
+
+			// here we have problem if the space i need is less than next size-meta so we should allocate
+
+			// This else not handeled in Test
+
+			else {
+				ptr->size = new_size;
+				ptr->prev_next_info.le_next->is_free = 0;
+				ptr->prev_next_info.le_next->size = 0;
+				struct BlockMetaData * tmpPtr = ptr->prev_next_info.le_next;
+				LIST_REMOVE(&memBlocks, tmpPtr);
+			}
+			return (void *) ((uint32) ptr + sizeOfMetaData());
+		}
+	} else if (new_size == ptr->size) {
+		return va;
+	}
+	//panic("realloc_block_FF is not implemented yet");
+	return NULL;
+}
